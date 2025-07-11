22a5033dd687921b9a51155e149ba403cda75be5
Vladlen Schetinin <vvschetinin@gmail.com>
2025-07-11 10:57:19 +0300

    first commit

---
+++ src/assets/ts/app.ts
@@ -0,0 +1,5 @@
+import "@/assets/ts/components/inputmask/main";
+import "@/assets/ts/components/myslider/main";
+import "@/assets/ts/components/formsend/formsend";
+import "@/assets/ts/main/script";
+import "@/assets/ts/components/lazyvideo/lazy-video";
