a88cefc72f2a58d8e6c11960c6fcec32678d56fb
Vladlen Schetinin <born-1965@mail.ru>
2025-12-24 07:28:37 +0300

    Модуль equalizer TS

--- src/assets/ts/app.ts
+++ src/assets/ts/app.ts
@@ -1,7 +1,8 @@
-import "@/assets/ts/components/inputmask/main";
-import "@/assets/ts/components/myslider/main";
 import "@/assets/ts/components/cookie/main";
+import "@/assets/ts/components/equalizer/equalizer";
 import "@/assets/ts/components/formsend/formsend";
+import "@/assets/ts/components/inputmask/main";
+import "@/assets/ts/components/lazyvideo/lazy-video";
+import "@/assets/ts/components/myslider/main";
 import "@/assets/ts/components/sendformtitle/sendformtitle";
 import "@/assets/ts/main/script";
-import "@/assets/ts/components/lazyvideo/lazy-video";
